15b0bd50336fb1c4a9c61f031d0644854e159e7f
Ryan <ryanrizkyf@gmail.com>
2021-02-20 15:50:43 +0700

    initialization commit

---
+++ EksplorasiData_DataPreProcessing/EksplorasiData_MemahamiData_Statistik_Pt_Satu.py
@@ -0,0 +1,21 @@
+# Tahapan-tahapan dalam pembuatan model machine learning.
+# Membuat model machine learning tidak serta-merta langsung modelling,
+# ada tahapan sebelumnya yang penting untuk dilakukan sehingga kita menghasilkan model yang baik.
+# Untuk penjelasan ini, kita akan mempraktekkan langsung ya. Kita akan memanfaatkan Pandas library.
+# Pandas cukup powerful untuk digunakan dalam menganalisa, memanipulasi dan membersihkan data.
+
+# Pertama- tama,  kita check dimensi data kita terlebih dahulu.
+# Silakan load datanya dan gunakan .shape, .head(), .info(), dan .describe()
+# untuk mengeksplorasi dataset secara berurut.
+
+# Dataset ini adalah data pembeli online yang mengunjungi website dari suatu e-commerce selama setahun,
+# yaitu 'https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/pythonTutorial/online_raw.csv'
+
+import pandas as pd
+dataset = pd.read_csv(
+    'online_raw.csv')
+print('Shape dataset:', dataset.shape)
+print('\nLima data teratas:\n', dataset.head())
+print('\nInformasi dataset:')
+print(dataset.info())
+print('\nStatistik deskriptif:\n', dataset.describe())
