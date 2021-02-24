cf2dbe30f6128c51d57ef091c0de02f17a2b3982
Ryan <ryanrizkyf@gmail.com>
2021-02-24 13:34:21 +0700

    edit commit

--- MiniProject/MiniProject.py
+++ MiniProject/MiniProject.py
@@ -21,6 +21,8 @@
 # yang sudah disharing sebelumnya. Jadi, tahap-tahap yang perlu dilakukan adalah (langkah ke-1) terlebih dahulu
 
 # Import library
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Baca data 'ecommerce_banner_promo.csv'
@@ -60,3 +62,20 @@ print(data.groupby('Clicked on Ad').size())
 # gunakan bins = data.Age.nunique() sebagai argumen. nunique() adalah fungsi untuk menghitung jumlah data
 # untuk setiap usia (Age).
 # Gunakan pairplot() dari seaborn modul untuk menggambarkan hubungan setiap feature.
+
+# Seting: matplotlib and seaborn
+sns.set_style('whitegrid')
+plt.style.use('fivethirtyeight')
+
+# 4. Data eksplorasi dengan visualisasi
+# 4a. Visualisasi Jumlah user dibagi ke dalam rentang usia (Age) menggunakan histogram (hist()) plot
+plt.figure(figsize=(10, 5))
+plt.hist(data['Age'], bins=data.Age.nunique())
+plt.xlabel('Age')
+plt.tight_layout()
+plt.show()
+
+# 4b. Gunakan pairplot() dari seaborn (sns) modul untuk menggambarkan hubungan setiap feature.
+plt.figure()
+sns.pairplot(data)
+plt.show()
